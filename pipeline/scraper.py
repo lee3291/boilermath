e7b9ba5ed659ca2449c78e700e5fce9602c02280
Ethan <122648917+lee3291@users.noreply.github.com>
2026-03-17 13:48:09 -0400

    implement web scraper

---
+++ pipeline/scraper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://www.math.purdue.edu/academic/courses/oldexams.php?course=MA16200"
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Success")
+else:
+    print(f"Request failed with status code: {response.status_code}")
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+all_links = soup.find_all("a")
+
+ma16200 = {}
+
+start_str = "https://www.math.purdue.edu"
+
+for link in all_links:
+    if link.get("href") and "serve_file.php" in link.get("href"):
+        pdfLink = link.get("href")
+        pdfName = pdfLink.split("file=")[1]
+        examName = pdfName.replace("Ans-", "").replace("Sol-", "").replace(".pdf", "")
+
+        if examName not in ma16200:
+            ma16200[examName] = {"exam": None, "answer": None, "solution": None}
+
+        examObj = ma16200[examName]
+
+        if "Ans" in pdfName:
+            examObj["answer"] = start_str + pdfLink
+        elif "Sol" in pdfName:
+            examObj["solution"] = start_str + pdfLink
+        else:
+            examObj["exam"] = start_str + pdfLink
+
+print(json.dumps(ma16200, indent=2))
