0730f3b51e60fd569971f2d9447d7196d7b5193e
Ethan <122648917+lee3291@users.noreply.github.com>
2026-03-18 21:54:22 -0400

    database: define schema and create tables in Neon

---
+++ pipeline/database.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
+from typing import Optional
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Exam(Base):
+    __tablename__ = "exam"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    course: Mapped[str] = mapped_column(String(7))
+    semester: Mapped[str] = mapped_column(String(5))
+    exam_number: Mapped[int] = mapped_column()
+
+
+class Problem(Base):
+    __tablename__ = "problem"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    exam_id: Mapped[int] = mapped_column(ForeignKey("exam.id"))
+    problem_number: Mapped[int] = mapped_column()
+    image_url: Mapped[str] = mapped_column()
+    answer: Mapped[Optional[str]] = (
+        mapped_column()
+    )  # optional for now but should not be nullible or optional eventually.
+
+
+class Tag(Base):
+    __tablename__ = "tag"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+
+class ProblemTag(Base):
+    __tablename__ = "problem_tag"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    problem_id: Mapped[int] = mapped_column(ForeignKey("problem.id"))
+    tag_id: Mapped[int] = mapped_column(ForeignKey("tag.id"))
+
+
+Base.metadata.create_all(engine)
